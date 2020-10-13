e616d6010cac83ae5f8246a587225e6d11e5aa71
FAHIM AHMED <fahimfahad35@gmail.com>
2020-10-13 16:45:37 +0600

    companies data getting

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const cors=require('cors');
-const MongoClient=require('mongodb').MongoClient;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectID;
 require('dotenv').config();
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port =5000;
+const port = 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.9obvp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9obvp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+console.log(process.env.DB_NAME,process.env.DB_PASS,process.env.DB_USER)
 
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('hlw everyone')
 })
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    const collection = client.db(`${process.env.DB_NAME}`).collection("companies");
+    console.log('database connected')
+
+    app.get('/allcompanies', (req, res) => {
+        collection.find({})
+          .toArray((err, documents) => {
+            res.send(documents);
+          })
+        console.log(err)
+        console.log('data loaded successfully')
+      })
 });
 
- app.listen(process.env.PORT||port);
+app.listen(process.env.PORT || port);
